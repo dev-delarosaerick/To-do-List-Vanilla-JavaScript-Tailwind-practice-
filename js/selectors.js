008eae55f84806f0ca86244ab9e101b8e2c085be
Erick de la Rosa <delarosa.erick1017@gmail.com>
2025-09-07 16:39:33 -0500

    feat: implement select elements and integrate with DOM

---
+++ js/selectors.js
@@ -0,0 +1,8 @@
+"use strict";
+
+export const titleInput = document.querySelector('#title');
+export const descriptionInput = document.querySelector('#description');
+
+export const form = document.querySelector('#notes-form');
+export const formInput = document.querySelector('#notes-form input[type="submit"]');
+export const notesContainer = document.querySelector('#notes');
