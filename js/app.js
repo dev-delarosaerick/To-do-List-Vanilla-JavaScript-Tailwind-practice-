349060478d44109063406bff7553059b47a187cb
Erick de la Rosa <delarosa.erick1017@gmail.com>
2025-09-07 19:22:10 -0500

    feat: db.js file added with config initializing indexedDB

--- js/app.js
+++ js/app.js
@@ -1,11 +1,14 @@
 "use strict";
 
+import { initDB } from "./db.js";
 import { notesData } from "./functions.js";
 import { descriptionInput, titleInput } from "./selectors.js";
 
-window.addEventListener('DOMContentLoaded', ()=> {
+window.onload = ()=> {
 
     titleInput.addEventListener('change', notesData);
     descriptionInput.addEventListener('change', notesData);
+    
+    initDB();
 
-});
+}
